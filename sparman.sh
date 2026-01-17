72bcf6db1116bb2f2698c2ce4bacb9e7f16545a8
Alexey Melezhik <melezhik@gmail.com>
2026-01-17 13:20:36 +0300

    sparman

--- sparman.sh
+++ sparman.sh
@@ -1,1 +1,4 @@
+sparman --env FORGEJO_API_TOKEN=$FORGEJO_API_TOKEN,FORGEJO_HOST=$FORGEJO_HOST  worker stop
 sparman --env FORGEJO_API_TOKEN=$FORGEJO_API_TOKEN,FORGEJO_HOST=$FORGEJO_HOST  worker start
+sparman worker_ui start
+
